96b3f2a46042bb25f084023c9d50ba7e3e7bd09c
Cata Puñales <cpunalesblanc@gmail.com>
2024-08-19 20:56:39 -0300

    Arreglo

--- js/index.js
+++ js/index.js
@@ -1,10 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-    document.addEventListener("DOMContentLoaded", function() {
         if (localStorage.getItem("sesionActiva") !== "true") {
             window.location.href = "login.html";
-        }
-    });
+        };
     
     document.getElementById("autos").addEventListener("click", function() {
         localStorage.setItem("catID", 101);
