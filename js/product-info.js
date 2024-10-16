54c8ec2eb19fcbb35ecdba0d720915083875494f
luciamamprelian <luciamamprelian@gmail.com>
2024-10-16 19:25:24 -0300

    Update product-info.js

--- js/product-info.js
+++ js/product-info.js
@@ -11,6 +11,33 @@ document.addEventListener('DOMContentLoaded', function () {
     // Recuperar el identificador del producto del almacenamiento local
     const productId = localStorage.getItem('selectedProductId');
 
+        // Crear un elemento de enlace para el CSS
+    const themeStyle = document.createElement('link');
+    themeStyle.rel = 'stylesheet';
+    document.head.appendChild(themeStyle);
+
+    // Cargar el tema preferido
+    const currentTheme = localStorage.getItem('theme');
+    if (currentTheme === 'dark') {
+        themeStyle.href = 'css/modo-noche.css'; // Ruta a tu CSS de modo oscuro
+        document.getElementById('toggle-theme').textContent = 'Modo Día';
+    } else {
+        themeStyle.href = 'css/products-info.css'; // Ruta a tu CSS de modo claro
+    }
+
+    // Evento para el botón de cambio de tema
+    document.getElementById('toggle-theme').addEventListener('click', function() {
+        if (themeStyle.href.includes('products-info.css')) {
+            themeStyle.href = 'css/modo-noche.css';
+            localStorage.setItem('theme', 'dark');
+            this.textContent = 'Modo Día';
+        } else {
+            themeStyle.href = 'css/products-info.css';
+            localStorage.setItem('theme', 'light');
+            this.textContent = 'Modo Noche';
+        }
+    });
+
     document.getElementById("btn-volver-listado").addEventListener("click", function() {
         window.location.href = "products.html"; // Redirige a products.html
     });
