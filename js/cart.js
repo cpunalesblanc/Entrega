3fd99ac7b36414785858eeca53bfc975348dce86
Cata Puñales <cpunalesblanc@gmail.com>
2024-11-01 20:09:30 -0300

    ahhhhhhhhhhhhhhh

--- js/cart.js
+++ js/cart.js
@@ -13,3 +13,22 @@ function stepper(btn){
 
     }
 }
+
+const productId = localStorage.getItem('selectedProductId');
+if (productId) {
+    // Hacer una solicitud a la API con el identificador del producto
+    fetch(`https://japceibal.github.io/emercado-api/products/${productId}.json`)
+        .then(response => response.json())
+        .then(productData => {
+            // Mostrar la información del producto en la página
+            document.querySelector('.nombre-producto').textContent = productData.name;
+            document.querySelector('.precio').textContent = `$${productData.cost}`;
+            // Mostrar la imagen principal desde la carpeta local 'img'
+            const imageFolderPath = 'img/';
+            document.querySelector('.foto').innerHTML = `<img src="${imageFolderPath}prod${productId}_1.jpg" alt="Imagen del producto" class="img-fluid main-image">`;        })
+        .catch(error => {
+            console.error("Error al obtener la información del producto:", error);
+        });
+} else {
+    console.error("No se encontró el ID del producto en el almacenamiento local.");
+}
