214876f98fe74e3ed24797fe269b297a27491bb0
bluerune <b.sofia.sosa@gmail.com>
2024-11-29 17:53:00 -0300

    Intento de agregar rutas

--- js/init.js
+++ js/init.js
@@ -7,13 +7,13 @@
 //const CART_BUY_URL = "https://japceibal.github.io/emercado-api/cart/buy.json";
 //const EXT_TYPE = ".json";
 
-const CATEGORIES_URL = "./api/cats/cat.json";
-const PUBLISH_PRODUCT_URL = "./api/sell/publish.json";
-const PRODUCTS_URL = "./api/cats_products/";
-const PRODUCT_INFO_URL = "./api/products/";
-const PRODUCT_INFO_COMMENTS_URL = "./api/products_comments/";
-const CART_INFO_URL = "./api/user_cart/";
-const CART_BUY_URL = "./api/cart/buy.json";
+const CATEGORIES_URL = require('./api/cats/cat.json');
+const PUBLISH_PRODUCT_URL = require('./api/sell/publish.json');
+const PRODUCTS_URL = require('./api/cats_products/');
+const PRODUCT_INFO_URL = require('./api/products/');
+const PRODUCT_INFO_COMMENTS_URL = require('./api/products_comments/');
+const CART_INFO_URL = require('./api/user_cart/');
+const CART_BUY_URL = require('./api/cart/buy.json');
 const EXT_TYPE = ".json";
 
 let showSpinner = function(){
--- server.js
+++ server.js
@@ -46,6 +46,43 @@ const usuarios = [
     return res.json({ token });
   });
 
+
+// ---------- Rutas ---------
+const CATEGORIES_URL = require('./api/cats/cat.json');
+const PUBLISH_PRODUCT_URL = require('./api/sell/publish.json');
+const PRODUCTS_URL = require('./api/cats_products/');
+const PRODUCT_INFO_URL = require('./api/products/');
+const PRODUCT_INFO_COMMENTS_URL = require('./api/products_comments/');
+const CART_INFO_URL = require('./api/user_cart/');
+const CART_BUY_URL = require('./api/cart/buy.json/');
+const EXT_TYPE = ".json";
+
+
+// Products
+app.get('/api/products', (req, res) => {
+  res.status(200).json(PRODUCT_INFO_URL);
+});
+
+// Products comments
+app.get('/api/products_comments', (req, res) => {
+  res.status(200).json(PRODUCT_INFO_COMMENTS_URL);
+});
+
+// Categorias
+app.get('/api/cats', (req, res) => {
+  res.status(200).json(CATEGORIES_URL);
+});
+
+// Categorias products
+app.get('/api/cats_products', (req, res) => {
+  res.status(200).json(PRODUCTS_URL);
+});
+
+// Cart
+app.get('/api/user_cart', (req, res) => {
+  res.status(200).json(CART_INFO_URL);
+});
+
 // Iniciar el servidor
 const PORT = 3000;
 app.listen(PORT, () => {
